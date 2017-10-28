a1bcfb72e7af8200a7f9efb96627ef551721e173
Jeff Button <jeffreyjbutton@gmail.com>
2017-10-27 21:33:09 -0700

    Timeservice for FreeCodeCamp

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+
+app.get("/", function (req, res) {
+    res.sendFile(process.cwd() + "/index.html");
+});
+
+// Route for input
+app.get("/:input", function (req, res) {
+    console.log("params", req.params.input);
+    res.send(parseInput(req.params.input));
+});
+
+function parseInput(input) {
+    let returnObj = { "unix": null, "natural": null };
+
+    // Attempt to process alpha string as a date
+    if (input.match(/[a-zA-Z]/)) {
+        const d = Date.parse(input);
+        if (!Number.isNaN(d)) {
+            returnObj.unix = d / 1000;
+            returnObj.natural = formatTimestamp(input);
+        }
+    } else {
+        // Process number as UNIX timestamp
+        const num = +input;
+        if (num > 0) {
+            returnObj.unix = num;
+            returnObj.natural = formatTimestamp(num * 1000);
+        }
+    }
+ 
+    return returnObj;
+}
+
+function formatTimestamp(timeStamp) {
+    const dateOptions = { year: 'numeric', month: 'long', day: 'numeric' };
+    return new Date(timeStamp).toLocaleDateString('en-US', dateOptions);
+}
+
+app.listen(3000, timeService);
+
+function timeService() {
+    console.log("Listening on port 3000");
+}
